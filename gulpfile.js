2b7e76f68d42e14cdcae399cfb750127bcfce57f
Aditya Rao <adirlzr@gmail.com>
2021-05-08 23:18:15 -0400

    feat: Setup live-reload using gulp and browsersync
    Signed-off-by: Aditya Rao <adirlzr@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
 
 sass.compiler = require('dart-sass');
+
 
 gulp.task('sass', function() {
   return gulp.src('./scss/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('.'))
+      .pipe(browserSync.stream())
 });
 
-gulp.task('sass:watch', function() {
-  return gulp.watch('./scss/*.scss', gulp.series(['sass']));
+gulp.task('serve', function() {
+  browserSync.init({
+    server: { baseDir: './' },
+    browser: ['firefox']
+  });
+
+  const reloadBrowser = function(done) {
+    browserSync.reload();
+    done();
+  }
+
+  gulp.watch(
+      ['./scss/*.scss', 'index.html'],
+      gulp.series(['sass', reloadBrowser])
+  );
 });
